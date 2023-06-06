3711608f8303ec4d46680f51e596b4c77f982a22
Gandham Dheeraj <121343901+Technoblade19876@users.noreply.github.com>
2023-06-06 09:58:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function PQP(){
+    window.location = "PQP.html";
+}
+
+function MQP(){
+    window.location = "MQP.html";
+}
+
+function IGKQ(){
+    window.location = "IGKQ.html";
+}
+
+function IMQ(){
+    window.location = "IMQ.html";
+}
