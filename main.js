1c93aa6e0f2b2b33d93b207abe78e516aca39ccc
Gandham Dheeraj <121343901+Technoblade19876@users.noreply.github.com>
2023-06-22 10:05:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,23 @@
-Webcam.set({
-    width:350,
-    height:300,
-    image_format: 'png',
-    png_quality:90
-});
-
-camera = document.getElementById('camera');
-
-Webcam.attach('#camera');
-
-function take_snapshot()
- {
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">'
-    });
+function PQP(){
+    window.location = "PQP.html";
 }
 
-    console.log('ml5 Version', ml5.version);
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jJojs31Ju/model.json',modelLoaded);
+function MQP(){
+    window.location = "MQP.html";
+}
 
-    function modelLoaded(){
-        console.log("Model loaded!");
-    }
+function IGKQ(){
+    window.location = "IGKQ.html";
+}
 
+function IMQ(){
+    window.location = "IMQ.html";
+}
 
-    function check(){
-        img = document.getElementById('selfie_image');
-        classifier.classify(img , gotResult);
-    }
+function RMQP(){
+    window.location = "RMQP.html";
+}
 
-    function gotResult(error , results){
-        if (error){
-            console.error(error);
-        }else{
-            console.log(results);
-            document.getElementById('object_name').innerHTML = results[0].label;
-            document.getElementById('object_accuracy').innerHTML = results[0].confidence.toFixed(2);
-        }
-    }
+function IGQ(){
+    window.location = "IGQ.html";
+}
